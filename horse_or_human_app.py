4ab2e0e020ec0167ca06cbb8749e87c776166afe
ggferreira <ggferreira2@gmail.com>
2022-05-02 17:18:22 -0300

    Changes on comments

--- horse_or_human_app.py
+++ horse_or_human_app.py
@@ -8,16 +8,19 @@ st.text("Insira a imagem de um Cavalo ou Ser Humano")
 
 
 uploaded_file = st.file_uploader("Escolha uma foto", type="jpg")
+# receives the image
 st.caption("Obs: a imagem tem que ser no formato .jpg")
 if uploaded_file is None:
     st.caption("Você ainda não inseriu nem uma imagem")
 
 else:
     image = Image.open(uploaded_file)
+    # shows the uploaded image
     st.image(image, caption='Foto escolhida', use_column_width=True)
     st.write("")
     st.write("Classificando...")
     label = image_classification(image, 'horse_or_human_model.h5')
+    # """takes the image and pass to the function calling the classifier model
     if label == 0:
         st.write("Provavelmente é a imagem de um Cavalo")
     else :
--- img_classification.py
+++ img_classification.py
@@ -4,24 +4,24 @@ import numpy as np
 
 
 def image_classification(img, weights_file):
-    # Load the model
-    model = keras.models.load_model(weights_file, compile=False)
 
-    # Create the array of the right shape to feed into the keras model
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    """Create the array of the right shape to keras model"""
     image = img
-    # image sizing
     size = (224, 224)
     image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
-    # turn the image into a numpy array
     image_array = np.asarray(image)
-    # Normalize the image
+    """turn the image into a numpy array"""
+
     normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
     # Load the image into the array
     data[0] = normalized_image_array
 
-    # run the inference
+    model = keras.models.load_model(weights_file, compile=False)
+    """the model(weights_file) has to be in the same folder as the app"""
+
     prediction = model.predict(data)
-    return np.argmax(prediction)  # return position of the highest probabili
+    return np.argmax(prediction)
+    """ run the prediction, then returns position of the highest probability"""
